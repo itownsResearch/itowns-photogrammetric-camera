bdfefa6fa96aba6bdb08cadf5da5490ba79102ab
nosy-b <ADevaux@ign.fr>
2019-06-12 18:24:22 +0200

    extracted specific menu

--- examples/js/OrientedImageHelper.js
+++ examples/js/OrientedImageHelper.js
@@ -138,27 +138,3 @@ function addCameraHelper(view, camera) {
     cameraHelper.updateMatrixWorld(true);
 }
 
-// eslint-disable-next-line no-unused-vars
-function setupPictureUI(menu, pictureInfos, plane, updateDistanceCallback, updateOpacityCallback, updateFOVCallback, view, min, max) {
-    var orientedImageGUI = menu.gui.addFolder('Oriented Image');
-
-    orientedImageGUI.add(pictureInfos, 'distance', min, max).name('Distance').onChange(function distanceChanged(value) {
-        pictureInfos.distance = value;
-        updateDistanceCallback(value);
-        view.notifyChange();
-    });
-
-    orientedImageGUI.add(pictureInfos, 'opacity', 0, 1).name('Opacity').onChange(function opacityChanged(value) {
-        pictureInfos.opacity = value;
-        updateOpacityCallback(value);
-        view.notifyChange();
-    });
-    
-    var pF = {zoom:1.};
-    orientedImageGUI.add( pF, 'zoom', 0.01, 2.).name('Zoom').onChange(function fovChanged(value) {
-        //pictureInfos.opacity = value;
-        updateFOVCallback(value);
-        view.notifyChange();
-    });
-
-}
